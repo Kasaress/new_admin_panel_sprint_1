cf079749d419f1020cf52a0ceec5f3d085f8a71b
Яна Бубнова <bujhvh@mail.ru>
2024-03-25 22:49:20 +0300

    ref

--- sqlite_to_postgres/constants.py
+++ sqlite_to_postgres/constants.py
@@ -1,12 +1,16 @@
 from data_objects import (FilmworkData, GenreData, GenreFilmworkData,
                           PersonData, PersonFilmworkData)
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 MAPPING_TABLE_DATACLASS = [
     ('genre', GenreData),
     ('person', PersonData),
-    # ('film_work', FilmworkData),
-    # ('person_film_work', PersonFilmworkData),
-    # ('genre_film_work', GenreFilmworkData),
+    ('film_work', FilmworkData),
+    ('person_film_work', PersonFilmworkData),
+    ('genre_film_work', GenreFilmworkData),
 ]
 
 CHUNK_SIZE = 10
@@ -16,3 +20,11 @@ SQL_INSERTS = """
         ({column_names_str}) VALUES {bind_values} 
         ON CONFLICT (id) DO NOTHING
       """
+
+DSL = {
+    'dbname': os.environ.get('DBNAME', 'movies_database'),
+    'user': os.environ.get('PG_USER', 'postgres'),
+    'password': os.environ.get('PASSWORD', 'postgres'),
+    'host': os.environ.get('HOST', '127.0.0.1'),
+    'port': os.environ.get('PORT', 5432)
+}
--- sqlite_to_postgres/load_data.py
+++ sqlite_to_postgres/load_data.py
@@ -3,7 +3,7 @@ import sqlite3
 import psycopg2
 from psycopg2.extras import DictCursor
 
-from constants import CHUNK_SIZE, MAPPING_TABLE_DATACLASS
+from constants import CHUNK_SIZE, MAPPING_TABLE_DATACLASS, DSL
 from exctactor import SQLiteExtractor
 from loader import PostgresSaver
 
@@ -50,14 +50,9 @@ def load_from_sqlite(connection: sqlite3.Connection, pg_conn):
 
 
 if __name__ == '__main__':
-    dsl = {
-        'dbname': 'movies_database',
-        'user': 'postgres',
-        'password': 'postgres',
-        'host': '127.0.0.1',
-        'port': 5432
-    }
-    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(
-            **dsl, cursor_factory=DictCursor
-    ) as pg_conn:
+    print(f'Запуск скрипта с конфигом: {DSL}')
+    with (
+        sqlite3.connect('db.sqlite') as sqlite_conn,
+        psycopg2.connect(**DSL, cursor_factory=DictCursor) as pg_conn
+    ):
         load_from_sqlite(sqlite_conn, pg_conn)
