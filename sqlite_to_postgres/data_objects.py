8bb2a8be3f923bbf538a33db657d431dc2828bd1
Яна Бубнова <bujhvh@mail.ru>
2024-03-26 09:24:39 +0300

    tests

--- sqlite_to_postgres/data_objects.py
+++ sqlite_to_postgres/data_objects.py
@@ -1,59 +1,53 @@
 from dataclasses import InitVar, dataclass
 from datetime import datetime
+from typing import Optional
 
 
 @dataclass
-class GenreData:
+class Base:
     id: str
-    name: str
     updated_at: InitVar[datetime] = None
     modified: datetime | None = None
     created: datetime | None = None
     created_at: InitVar[datetime] = None
-    description: str | None = None
 
-    def __post_init__(self, created_at, updated_at):
+    @staticmethod
+    def _parse_datetime(dt_str: str) -> Optional[datetime]:
+        try:
+            return datetime.fromisoformat(dt_str)
+        except ValueError:
+            return None
+
+    def __post_init__(self, created_at, updated_at, file_path=None):
         if created_at:
             self.created = created_at
         if updated_at:
             self.modified = updated_at
+        if isinstance(self.modified, str):
+            self.modified = self._parse_datetime(self.modified)
+        if isinstance(self.created, str):
+            self.created = self._parse_datetime(self.created)
 
 
 @dataclass
-class FilmworkData:
-    id: str
-    title: str
-    type: str
+class GenreData(Base):
+    name: str = ''
+    description: str | None = None
+
+
+@dataclass
+class FilmworkData(Base):
+    title: str = ''
+    type: str = ''
     description: str | None = None
     creation_date: datetime | None = None
     rating: float | None = None
     file_path: InitVar[str] = None
-    updated_at: InitVar[datetime] = None
-    modified: datetime | None = None
-    created: datetime | None = None
-    created_at: InitVar[datetime] = None
-
-    def __post_init__(self, file_path, updated_at, created_at):
-        if created_at:
-            self.created = created_at
-        if updated_at:
-            self.modified = updated_at
 
 
 @dataclass
-class PersonData:
-    id: str
-    full_name: str
-    updated_at: InitVar[datetime] = None
-    modified: datetime | None = None
-    created: datetime | None = None
-    created_at: InitVar[datetime] = None
-
-    def __post_init__(self, created_at, updated_at):
-        if created_at:
-            self.created = created_at
-        if updated_at:
-            self.modified = updated_at
+class PersonData(Base):
+    full_name: str = ''
 
 
 @dataclass
--- sqlite_to_postgres/tests_load_data.py
+++ sqlite_to_postgres/tests_load_data.py
@@ -4,7 +4,7 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 from constants import CHUNK_SIZE, DSL, MAPPING_TABLE_DATACLASS
-
+from dataclasses import dataclass
 
 class TestDataLoad:
     def __init__(self, sqlite_conn, pg_conn) -> None:
@@ -24,6 +24,38 @@ class TestDataLoad:
         result = cursor.fetchone()
         return result[0]
 
+    def _get_sqlite_data(self, table_name, data_class, start_row, end_row):
+        self.sqlite_conn.row_factory = sqlite3.Row
+        cursor = self.sqlite_conn.cursor()
+        query = f"SELECT * FROM {table_name} LIMIT ?, ?"
+        try:
+            cursor.execute(query, (start_row, end_row - start_row))
+            result = cursor.fetchall()
+            print(
+                f'Данные получены из SQLite: таблица {table_name}, '
+                f'строки с {start_row} по {end_row}'
+            )
+            return [data_class(**data) for data in result]
+        except Exception as error:
+            print(f'Ошибка чтения данных из таблицы {table_name}: {error}')
+            return None
+
+    def _get_postgres_data(self, table_name, data_class, start_row, end_row):
+        cursor = self.pg_conn.cursor()
+        query = "SELECT * FROM content.{table_name} OFFSET %s FETCH FIRST %s ROWS ONLY".format(table_name=table_name)
+
+        try:
+            cursor.execute(query, (start_row, end_row - start_row))
+            result = cursor.fetchall()
+            print(
+                f'Данные получены из Postgres: таблица {table_name}, '
+                f'строки с {start_row} по {end_row}'
+            )
+            return [data_class(**data) for data in result]
+        except Exception as error:
+            print(f'Ошибка чтения данных из таблицы {table_name}: {error}')
+            return None
+
     def check_rows_counts(self, table_name: str) -> None:
         sqlite_row_count = self._get_sqlite_row_count(table_name)
         postgres_row_count = self._get_postgres_row_count(table_name)
@@ -34,9 +66,32 @@ class TestDataLoad:
                                                         f'в Postgres {postgres_row_count}')
         print(f'количество строк в таблицах {table_name} совпадает')
 
+    def check_data_equal(self, table_name: str, data_class: dataclass) -> None:
+        count = self._get_sqlite_row_count(table_name)
+        chunk_size = CHUNK_SIZE
+        start = 0
+        while start < count:
+            end = start + chunk_size
+            if end > count:
+                end = count
+            sqlite_data = self._get_sqlite_data(table_name, data_class, start, end)
+            postgres_data = self._get_postgres_data(table_name, data_class, start, end)
+            print(f'{sqlite_data[0].modified}')
+            print(f'{type(sqlite_data[0].modified)})')
+            print(f'{postgres_data[0].modified}')
+            print(f'{type(postgres_data[0].modified)}')
+            assert sqlite_data[0].modified == postgres_data[0].modified
+            assert sqlite_data == postgres_data, (f'Не совпадают данные в таблицах {table_name}. '
+                                                  f'{sqlite_data=}, '
+                                                  f'{postgres_data=}.')
+            start = end
+
+        print(f'Данные в таблицах {table_name} совпадают.')
+
     def run(self):
-        for table_name, _ in MAPPING_TABLE_DATACLASS:
+        for table_name, data_class in MAPPING_TABLE_DATACLASS:
             self.check_rows_counts(table_name)
+            self.check_data_equal(table_name, data_class)
 
 
 if __name__ == '__main__':
